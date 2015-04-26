cf8f7cd0470bc90b9b274cdd14f63bb3a12b206c
Max Headley <max@jet.com>
2015-04-26 00:27:48 -0400

    Initial commit

---
+++ WebTutorial/Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+namespace WebTutorial.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebTutorial.Models.TodoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebTutorial.Models.TodoContext context)
+        {
+            var r = new Random();
+            var items = Enumerable.Range(1, 50).Select(o => new WebTutorial.Models.Todo {
+                DueDate = new DateTime(2012, r.Next(1, 12), r.Next(1, 28)),
+                Priority = (byte)r.Next(10),
+                Text = o.ToString()
+            }).ToArray();
+            context.Todoes.AddOrUpdate(item => new { item.Text }, items);
+        }
+    }
+}
